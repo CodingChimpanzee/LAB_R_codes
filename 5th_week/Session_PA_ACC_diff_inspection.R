7c6ffe42fbc20ff65619bc5bda1bd3143e7c2ab4
unknown <kanghyun51015@gm.gist.ac.kr>
2021-07-19 19:02:50 +0900

    7/19

--- 5th_week/Session_PA_ACC_diff_inspection.R
+++ 5th_week/Session_PA_ACC_diff_inspection.R
@@ -1,14 +1,13 @@
-# See phasic alertness task accuracy in session period
-# It does not see the accuracy difference between tendencies
+# See phasic alertness task accuracy difference in session period (for 11 participants)
 
 #import library
 library(data.table)
 library(ggplot2)
 setwd("C:/Users/Biocomputing/Documents")
 
-PPC <- fread("C:/Users/Biocomputing/Documents/CSV_DATA/PPC.csv", header = T)
-Sham <- fread("C:/Users/Biocomputing/Documents/CSV_DATA/Sham.csv", header = T)
-block <- fread("C:/Users/Biocomputing/Documents/CSV_DATA/blocks.csv", header = T)
+block <- fread("C:/Users/Biocomputing/Documents/CSV_DATA/inspection_blocks.csv", header = T)
+Part_N <- fread("C:/Users/Biocomputing/Documents/CSV_DATA/Participants_N.csv", header = T)
+Part_PPC <- fread("C:/Users/Biocomputing/Documents/CSV_DATA/Participants_PPC.csv", header = T)
 
 #import files
 S1_N <- list.files("CSV_DATA/Session_1_N")
@@ -46,6 +45,9 @@ for(file in S1_N){
   temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
                  by = Warnsound]
+  temp <- abs(temp[1,2]-temp[2,2])
+  temp <- as.data.table(temp)
+  temp <- setnames(temp, "accuracy", "ACC_mean_diff_each")
   S1_PACC_N <- rbind(S1_PACC_N, temp)
 }
 PAS_ACC_N <- rbind(PAS_ACC_N, S1_PACC_N)
@@ -57,6 +59,9 @@ for(file in S1_PPC){
   temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
                  by = Warnsound]
+  temp <- abs(temp[1,2]-temp[2,2])
+  temp <- as.data.table(temp)
+  temp <- setnames(temp, "accuracy", "ACC_mean_diff_each")
   S1_PACC_PPC <- rbind(S1_PACC_PPC, temp)
 }
 PAS_ACC_PPC <- rbind(PAS_ACC_PPC, S1_PACC_PPC)
@@ -69,6 +74,9 @@ for(file in S2_N){
   temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
                  by = Warnsound]
+  temp <- abs(temp[1,2]-temp[2,2])
+  temp <- as.data.table(temp)
+  temp <- setnames(temp, "accuracy", "ACC_mean_diff_each")
   S2_PACC_N <- rbind(S2_PACC_N, temp)
 }
 PAS_ACC_N <- rbind(PAS_ACC_N, S2_PACC_N)
@@ -80,6 +88,9 @@ for(file in S2_PPC){
   temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
                  by = Warnsound]
+  temp <- abs(temp[1,2]-temp[2,2])
+  temp <- as.data.table(temp)
+  temp <- setnames(temp, "accuracy", "ACC_mean_diff_each")
   S2_PACC_PPC <- rbind(S2_PACC_PPC, temp)
 }
 PAS_ACC_PPC <- rbind(PAS_ACC_PPC, S2_PACC_PPC)
@@ -92,6 +103,9 @@ for(file in S3_N){
   temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
                  by = Warnsound]
+  temp <- abs(temp[1,2]-temp[2,2])
+  temp <- as.data.table(temp)
+  temp <- setnames(temp, "accuracy", "ACC_mean_diff_each")
   S3_PACC_N <- rbind(S3_PACC_N, temp)
 }
 PAS_ACC_N <- rbind(PAS_ACC_N, S3_PACC_N)
@@ -103,7 +117,11 @@ for(file in S3_PPC){
   temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
                  by = Warnsound]
+  temp <- abs(temp[1,2]-temp[2,2])
+  temp <- as.data.table(temp)
+  temp <- setnames(temp, "accuracy", "ACC_mean_diff_each")
   S3_PACC_PPC <- rbind(S3_PACC_PPC, temp)
+  
 }
 PAS_ACC_PPC <- rbind(PAS_ACC_PPC, S3_PACC_PPC)
 #------------------------------------------------------------------------------#
@@ -115,6 +133,9 @@ for(file in S4_N){
   temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
                  by = Warnsound]
+  temp <- abs(temp[1,2]-temp[2,2])
+  temp <- as.data.table(temp)
+  temp <- setnames(temp, "accuracy", "ACC_mean_diff_each")
   S4_PACC_N <- rbind(S4_PACC_N, temp)
 }
 PAS_ACC_N <- rbind(PAS_ACC_N, S4_PACC_N)
@@ -126,6 +147,9 @@ for(file in S4_PPC){
   temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
                  by = Warnsound]
+  temp <- abs(temp[1,2]-temp[2,2])
+  temp <- as.data.table(temp)
+  temp <- setnames(temp, "accuracy", "ACC_mean_diff_each")
   S4_PACC_PPC <- rbind(S4_PACC_PPC, temp)
 }
 PAS_ACC_PPC <- rbind(PAS_ACC_PPC, S4_PACC_PPC)
@@ -138,6 +162,9 @@ for(file in S5_N){
   temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
                  by = Warnsound]
+  temp <- abs(temp[1,2]-temp[2,2])
+  temp <- as.data.table(temp)
+  temp <- setnames(temp, "accuracy", "ACC_mean_diff_each")
   S5_PACC_N <- rbind(S5_PACC_N, temp)
 }
 PAS_ACC_N <- rbind(PAS_ACC_N, S5_PACC_N)
@@ -149,6 +176,9 @@ for(file in S5_PPC){
   temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
                  by = Warnsound]
+  temp <- abs(temp[1,2]-temp[2,2])
+  temp <- as.data.table(temp)
+  temp <- setnames(temp, "accuracy", "ACC_mean_diff_each")
   S5_PACC_PPC <- rbind(S5_PACC_PPC, temp)
 }
 PAS_ACC_PPC <- rbind(PAS_ACC_PPC, S5_PACC_PPC)
@@ -161,6 +191,9 @@ for(file in S6_N){
   temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
                  by = Warnsound]
+  temp <- abs(temp[1,2]-temp[2,2])
+  temp <- as.data.table(temp)
+  temp <- setnames(temp, "accuracy", "ACC_mean_diff_each")
   S6_PACC_N <- rbind(S6_PACC_N, temp)
 }
 PAS_ACC_N <- rbind(PAS_ACC_N, S6_PACC_N)
@@ -172,195 +205,72 @@ for(file in S6_PPC){
   temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
                  by = Warnsound]
+  temp <- abs(temp[1,2]-temp[2,2])
+  temp <- as.data.table(temp)
+  temp <- setnames(temp, "accuracy", "ACC_mean_diff_each")
   S6_PACC_PPC <- rbind(S6_PACC_PPC, temp)
 }
 PAS_ACC_PPC <- rbind(PAS_ACC_PPC, S6_PACC_PPC)
 #------------------------------------------------------------------------------#
-
-
-
-#------------------------------------------------------------------------------#
 # Accuracy tendency by sessions
-session_bind_N <- NULL
-session_bind_PPC <- NULL
+session_bind_PACC_N <- NULL
+session_bind_PACC_PPC <- NULL
 SR_N <- NULL
 SR_PPC <- NULL
 
 #------------------------------------------------------------------------------#
 # For Session 1
-SR_N <- S1_PACC_N[,
-                  .(sr = sd(accuracy)/sqrt(11)),
-]
-SR_PPC <- S1_PACC_PPC[,
-                      .(sr = sd(accuracy)/sqrt(11)),
-]
 
-# P value
-S1_PACC_N_P <- cbind(Sham ,S1_PACC_N)
-S1_PACC_PPC_P <- cbind(PPC ,S1_PACC_PPC)
-P1 <- rbind(S1_PACC_N_P, S1_PACC_PPC_P)
-res1 <- t.test(accuracy~tDCS, data=P1,
-               paired = TRUE, var.equal = FALSE)
-
-
-S1_PACC_N <- S1_PACC_N[,
-                       .(accuracy = mean(accuracy)),
-]
-S1_PACC_PPC <- S1_PACC_PPC[,
-                           .(accuracy = mean(accuracy)),
-]
-session_bind_N <- rbind(session_bind_N, S1_PACC_N)
-session_bind_PPC <- rbind(session_bind_PPC, S1_PACC_PPC)
+session_bind_PACC_N <- rbind(session_bind_PACC_N, S1_PACC_N)
+session_bind_PACC_PPC <- rbind(session_bind_PACC_PPC, S1_PACC_PPC)
 
 #------------------------------------------------------------------------------#
 # For Session 2
-SR_N <- rbind(SR_N, S2_PACC_N[,
-                              .(sr = sd(accuracy)/sqrt(11)),
-])
-SR_PPC <- rbind(SR_PPC, S2_PACC_PPC[,
-                                    .(sr = sd(accuracy)/sqrt(11)),
-])
 
-# P value
-S2_PACC_N_P <- cbind(Sham ,S2_PACC_N)
-S2_PACC_PPC_P <- cbind(PPC ,S2_PACC_PPC)
-P2 <- rbind(S2_PACC_N_P, S2_PACC_PPC_P)
-res2 <- t.test(accuracy~tDCS, data=P2,
-               paired = TRUE, var.equal = FALSE)
-
-S2_PACC_N <- S2_PACC_N[,
-                       .(accuracy = mean(accuracy)),
-]
-S2_PACC_PPC <- S2_PACC_PPC[,
-                           .(accuracy = mean(accuracy)),
-]
-session_bind_N <- rbind(session_bind_N, S2_PACC_N)
-session_bind_PPC <- rbind(session_bind_PPC, S2_PACC_PPC)
+session_bind_PACC_N <- rbind(session_bind_PACC_N, S2_PACC_N)
+session_bind_PACC_PPC <- rbind(session_bind_PACC_PPC, S2_PACC_PPC)
 
 #------------------------------------------------------------------------------#
 # For Session 3
-SR_N <- rbind(SR_N, S3_PACC_N[,
-                              .(sr = sd(accuracy)/sqrt(11)),
-])
-SR_PPC <- rbind(SR_PPC, S3_PACC_PPC[,
-                                    .(sr = sd(accuracy)/sqrt(11)),
-])
 
-# P value
-S3_PACC_N_P <- cbind(Sham ,S3_PACC_N)
-S3_PACC_PPC_P <- cbind(PPC ,S3_PACC_PPC)
-P3 <- rbind(S3_PACC_N_P, S3_PACC_PPC_P)
-res3 <- t.test(accuracy~tDCS, data=P3, 
-               paired = TRUE, var.equal = FALSE)
-
-S3_PACC_N <- S3_PACC_N[,
-                       .(accuracy = mean(accuracy)),
-]
-S3_PACC_PPC <- S3_PACC_PPC[,
-                           .(accuracy = mean(accuracy)),
-]
-session_bind_N <- rbind(session_bind_N, S3_PACC_N)
-session_bind_PPC <- rbind(session_bind_PPC, S3_PACC_PPC)
+session_bind_PACC_N <- rbind(session_bind_PACC_N, S3_PACC_N)
+session_bind_PACC_PPC <- rbind(session_bind_PACC_PPC, S3_PACC_PPC)
 
 #------------------------------------------------------------------------------#
 # For Session 4
-SR_N <- rbind(SR_N, S4_PACC_N[,
-                              .(sr = sd(accuracy)/sqrt(11)),
-])
-SR_PPC <- rbind(SR_PPC, S4_PACC_PPC[,
-                                    .(sr = sd(accuracy)/sqrt(11)),
-])
 
-# P value
-S4_PACC_N_P <- cbind(Sham ,S4_PACC_N)
-S4_PACC_PPC_P <- cbind(PPC ,S4_PACC_PPC)
-P4 <- rbind(S4_PACC_N_P, S4_PACC_PPC_P)
-res4 <- t.test(accuracy~tDCS, data=P4, 
-               paired = TRUE, var.equal = FALSE)
-
-S4_PACC_N <- S4_PACC_N[,
-                       .(accuracy = mean(accuracy)),
-]
-S4_PACC_PPC <- S4_PACC_PPC[,
-                           .(accuracy = mean(accuracy)),
-]
-session_bind_N <- rbind(session_bind_N, S4_PACC_N)
-session_bind_PPC <- rbind(session_bind_PPC, S4_PACC_PPC)
+session_bind_PACC_N <- rbind(session_bind_PACC_N, S4_PACC_N)
+session_bind_PACC_PPC <- rbind(session_bind_PACC_PPC, S4_PACC_PPC)
 
 #------------------------------------------------------------------------------#
 # For Session 5
-SR_N <- rbind(SR_N, S5_PACC_N[,
-                              .(sr = sd(accuracy)/sqrt(11)),
-])
-SR_PPC <- rbind(SR_PPC, S5_PACC_PPC[,
-                                    .(sr = sd(accuracy)/sqrt(11)),
-])
 
-# P value
-S5_PACC_N_P <- cbind(Sham ,S5_PACC_N)
-S5_PACC_PPC_P <- cbind(PPC ,S5_PACC_PPC)
-P5 <- rbind(S5_PACC_N_P, S5_PACC_PPC_P)
-res5 <- t.test(accuracy~tDCS, data=P5,
-               paired = TRUE, var.equal = FALSE)
-
-S5_PACC_N <- S5_PACC_N[,
-                       .(accuracy = mean(accuracy)),
-]
-S5_PACC_PPC <- S5_PACC_PPC[,
-                           .(accuracy = mean(accuracy)),
-]
-session_bind_N <- rbind(session_bind_N, S5_PACC_N)
-session_bind_PPC <- rbind(session_bind_PPC, S5_PACC_PPC)
+session_bind_PACC_N <- rbind(session_bind_PACC_N, S5_PACC_N)
+session_bind_PACC_PPC <- rbind(session_bind_PACC_PPC, S5_PACC_PPC)
 
 #------------------------------------------------------------------------------#
 # For Session 6
-SR_N <- rbind(SR_N, S6_PACC_N[,
-                              .(sr = sd(accuracy)/sqrt(11)),
-])
-SR_PPC <- rbind(SR_PPC, S6_PACC_PPC[,
-                                    .(sr = sd(accuracy)/sqrt(11)),
-])
 
-# P value
-S6_PACC_N_P <- cbind(Sham ,S6_PACC_N)
-S6_PACC_PPC_P <- cbind(PPC ,S6_PACC_PPC)
-P6 <- rbind(S6_PACC_N_P, S6_PACC_PPC_P)
-res6 <- t.test(accuracy~tDCS, data=P6,
-               paired = TRUE, var.equal = FALSE)
-
-S6_PACC_N <- S6_PACC_N[,
-                       .(accuracy = mean(accuracy)),
-]
-S6_PACC_PPC <- S6_PACC_PPC[,
-                           .(accuracy = mean(accuracy)),
-]
-session_bind_N <- rbind(session_bind_N, S6_PACC_N)
-session_bind_PPC <- rbind(session_bind_PPC, S6_PACC_PPC)
+session_bind_PACC_N <- rbind(session_bind_PACC_N, S6_PACC_N)
+session_bind_PACC_PPC <- rbind(session_bind_PACC_PPC, S6_PACC_PPC)
 
 
-session_bind_N <- cbind(Sham, session_bind_N, SR_N)
-session_bind_PPC <- cbind(PPC, session_bind_PPC, SR_PPC)
-
-session_bind_all <- rbind(session_bind_N, session_bind_PPC)
-session_bind_all <- cbind(block, session_bind_all)
+# bind using cbind function
+session_bind_PACC_N <- cbind(block, session_bind_PACC_N, Part_N)
+session_bind_PACC_PPC <- cbind(block, session_bind_PACC_PPC, Part_PPC)
 #------------------------------------------------------------------------------#
 # ACC tendency between sessions (in graphs)
 
-
-g1 <- ggplot(data = session_bind_all,
-             aes(x = Blocks, y = accuracy, group = tDCS, color = tDCS)) + geom_line(size=1)
-g1 <- g1 + geom_errorbar(aes(ymin = accuracy-sr, ymax = accuracy+sr), width = 0.2) + geom_point(size = 3)
-g1 <- g1 + ggtitle("Simple accuracy in phasic alertness task") + theme(plot.title = element_text(hjust=0.5))
-#g1 <- g1 + annotate(geom="text", x=6, y=1, label="p=0.5")
+# SHAM Stimulation
+g1 <- ggplot(data = session_bind_PACC_N,
+             aes(x = Blocks, y = ACC_mean_diff_each, group = Participants_N, color = Participants_N)) + geom_line(size=1)
+g1 <- g1 + geom_point(size = 3)
+g1 <- g1 + ggtitle("SHAM stimulation accuracy in phasic alertness task") + theme(plot.title = element_text(hjust=0.5))
 plot(g1)
 
-# P value based on student paired t-test
-
-print(res1$p.value)
-print(res2$p.value)
-print(res3$p.value)
-print(res4$p.value)
-print(res5$p.value)
-print(res6$p.value)
-
-# https://blog.naver.com/nife0719/221000580841
+# PPC Stimulation
+g2 <- ggplot(data = session_bind_PACC_PPC,
+             aes(x = Blocks, y = ACC_mean_diff_each, group = Participants_PPC, color = Participants_PPC)) + geom_line(size=1)
+g2 <- g2 + geom_point(size = 3)
+g2 <- g2 + ggtitle("PPC stimulation accuracy in phasic alertness task") + theme(plot.title = element_text(hjust=0.5))
+plot(g2)
--- 5th_week/Session_PA_ACC_simple_inspection.R
+++ 5th_week/Session_PA_ACC_simple_inspection.R
@@ -1,4 +1,4 @@
-# See Executive Vigilance task accuracy(ACC) in session period
+# See phasic alertness task accuracy difference in session period (for 11 participants)
 
 #import library
 library(data.table)
@@ -28,204 +28,200 @@ S5_PPC <- list.files("CSV_DATA/Session_5_PPC")
 S6_N <- list.files("CSV_DATA/Session_6_N")
 S6_PPC <- list.files("CSV_DATA/Session_6_PPC")
 
-#Executive Vigilance tendency
+#Phasic Alertness tendency
+
+#PPC tDCS PA ACC value in Session
+PAS_ACC_PPC <- NULL
+
+#Sham tDCS PA ACC value in Session
+PAS_ACC_N <- NULL
+
 #------------------------------------------------------------------------------#
 # For Session 1
-S1_EVACC_N <- NULL
+S1_PACC_N <- NULL
 setwd("C:/Users/Biocomputing/Documents/CSV_DATA/Session_1_N")
 for(file in S1_N){
   temp <- fread(file, header=T)
-  temp <- (temp)[Correct=="SPACE",
+  temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
-  ]
-  S1_EVACC_N <- rbind(S1_EVACC_N, temp)
+                 ]
+  S1_PACC_N <- rbind(S1_PACC_N, temp)
 }
 
-S1_EVACC_PPC <- NULL
+S1_PACC_PPC <- NULL
 setwd("C:/Users/Biocomputing/Documents/CSV_DATA/Session_1_PPC")
 for(file in S1_PPC){
   temp <- fread(file, header=T)
-  temp <- (temp)[Correct=="SPACE",
+  temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
-  ]
-  S1_EVACC_PPC <- rbind(S1_EVACC_PPC, temp)
+                 ]
+  S1_PACC_PPC <- rbind(S1_PACC_PPC, temp)
 }
-
 #------------------------------------------------------------------------------#
 # For Session 2
-S2_EVACC_N <- NULL
+S2_PACC_N <- NULL
 setwd("C:/Users/Biocomputing/Documents/CSV_DATA/Session_2_N")
 for(file in S2_N){
   temp <- fread(file, header=T)
-  temp <- (temp)[Correct=="SPACE",
+  temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
-  ]
-  S2_EVACC_N <- rbind(S2_EVACC_N, temp)
+                 ]
+  S2_PACC_N <- rbind(S2_PACC_N, temp)
 }
 
-S2_EVACC_PPC <- NULL
+S2_PACC_PPC <- NULL
 setwd("C:/Users/Biocomputing/Documents/CSV_DATA/Session_2_PPC")
 for(file in S2_PPC){
   temp <- fread(file, header=T)
-  temp <- (temp)[Correct=="SPACE",
+  temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
-  ]
-  S2_EVACC_PPC <- rbind(S2_EVACC_PPC, temp)
+                 ]
+  S2_PACC_PPC <- rbind(S2_PACC_PPC, temp)
 }
-
 #------------------------------------------------------------------------------#
 # For Session 3
-S3_EVACC_N <- NULL
+S3_PACC_N <- NULL
 setwd("C:/Users/Biocomputing/Documents/CSV_DATA/Session_3_N")
 for(file in S3_N){
   temp <- fread(file, header=T)
-  temp <- (temp)[Correct=="SPACE",
+  temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
-  ]
-  S3_EVACC_N <- rbind(S3_EVACC_N, temp)
+                ]
+  S3_PACC_N <- rbind(S3_PACC_N, temp)
 }
 
-S3_EVACC_PPC <- NULL
+S3_PACC_PPC <- NULL
 setwd("C:/Users/Biocomputing/Documents/CSV_DATA/Session_3_PPC")
 for(file in S3_PPC){
   temp <- fread(file, header=T)
-  temp <- (temp)[Correct=="SPACE",
+  temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
-  ]
-  S3_EVACC_PPC <- rbind(S3_EVACC_PPC, temp)
+                 ]
+  S3_PACC_PPC <- rbind(S3_PACC_PPC, temp)
 }
-
 #------------------------------------------------------------------------------#
 # For Session 4
-S4_EVACC_N <- NULL
+S4_PACC_N <- NULL
 setwd("C:/Users/Biocomputing/Documents/CSV_DATA/Session_4_N")
 for(file in S4_N){
   temp <- fread(file, header=T)
-  temp <- (temp)[Correct=="SPACE",
+  temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
-  ]
-  S4_EVACC_N <- rbind(S4_EVACC_N, temp)
+                 ]
+  S4_PACC_N <- rbind(S4_PACC_N, temp)
 }
 
-S4_EVACC_PPC <- NULL
+S4_PACC_PPC <- NULL
 setwd("C:/Users/Biocomputing/Documents/CSV_DATA/Session_4_PPC")
 for(file in S4_PPC){
   temp <- fread(file, header=T)
-  temp <- (temp)[Correct=="SPACE",
+  temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
-  ]
-  S4_EVACC_PPC <- rbind(S4_EVACC_PPC, temp)
+                 ]
+  S4_PACC_PPC <- rbind(S4_PACC_PPC, temp)
 }
-
 #------------------------------------------------------------------------------#
 # For Session 5
-S5_EVACC_N <- NULL
+S5_PACC_N <- NULL
 setwd("C:/Users/Biocomputing/Documents/CSV_DATA/Session_5_N")
 for(file in S5_N){
   temp <- fread(file, header=T)
-  temp <- (temp)[Correct=="SPACE",
+  temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
-  ]
-  S5_EVACC_N <- rbind(S5_EVACC_N, temp)
+                 ]
+  S5_PACC_N <- rbind(S5_PACC_N, temp)
 }
 
-S5_EVACC_PPC <- NULL
+S5_PACC_PPC <- NULL
 setwd("C:/Users/Biocomputing/Documents/CSV_DATA/Session_5_PPC")
 for(file in S5_PPC){
   temp <- fread(file, header=T)
-  temp <- (temp)[Correct=="SPACE",
+  temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
-  ]
-  S5_EVACC_PPC <- rbind(S5_EVACC_PPC, temp)
+                 ]
+  S5_PACC_PPC <- rbind(S5_PACC_PPC, temp)
 }
-
 #------------------------------------------------------------------------------#
 # For Session 6
-S6_EVACC_N <- NULL
+S6_PACC_N <- NULL
 setwd("C:/Users/Biocomputing/Documents/CSV_DATA/Session_6_N")
 for(file in S6_N){
   temp <- fread(file, header=T)
-  temp <- (temp)[Correct=="SPACE",
+  temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
-  ]
-  S6_EVACC_N <- rbind(S6_EVACC_N, temp)
+                 ]
+  S6_PACC_N <- rbind(S6_PACC_N, temp)
 }
 
-S6_EVACC_PPC <- NULL
+S6_PACC_PPC <- NULL
 setwd("C:/Users/Biocomputing/Documents/CSV_DATA/Session_6_PPC")
 for(file in S6_PPC){
   temp <- fread(file, header=T)
-  temp <- (temp)[Correct=="SPACE",
+  temp <- (temp)[Correct!="SPACE",
                  .(accuracy = mean(PracStimulus.ACC)),
-  ]
-  S6_EVACC_PPC <- rbind(S6_EVACC_PPC, temp)
+                 ]
+  S6_PACC_PPC <- rbind(S6_PACC_PPC, temp)
 }
-
 #------------------------------------------------------------------------------#
 # Accuracy tendency by sessions
-session_bindEVACC_N <- NULL
-session_bindEVACC_PPC <- NULL
+session_bind_PACC_N <- NULL
+session_bind_PACC_PPC <- NULL
 SR_N <- NULL
 SR_PPC <- NULL
-
 
 #------------------------------------------------------------------------------#
 # For Session 1
 
-
-session_bindEVACC_N <- rbind(session_bindEVACC_N, S1_EVACC_N)
-session_bindEVACC_PPC <- rbind(session_bindEVACC_PPC, S1_EVACC_PPC)
+session_bind_PACC_N <- rbind(session_bind_PACC_N, S1_PACC_N)
+session_bind_PACC_PPC <- rbind(session_bind_PACC_PPC, S1_PACC_PPC)
 
 #------------------------------------------------------------------------------#
 # For Session 2
 
-
-session_bindEVACC_N <- rbind(session_bindEVACC_N, S2_EVACC_N)
-session_bindEVACC_PPC <- rbind(session_bindEVACC_PPC, S2_EVACC_PPC)
+session_bind_PACC_N <- rbind(session_bind_PACC_N, S2_PACC_N)
+session_bind_PACC_PPC <- rbind(session_bind_PACC_PPC, S2_PACC_PPC)
 
 #------------------------------------------------------------------------------#
 # For Session 3
 
-session_bindEVACC_N <- rbind(session_bindEVACC_N, S3_EVACC_N)
-session_bindEVACC_PPC <- rbind(session_bindEVACC_PPC, S3_EVACC_PPC)
+session_bind_PACC_N <- rbind(session_bind_PACC_N, S3_PACC_N)
+session_bind_PACC_PPC <- rbind(session_bind_PACC_PPC, S3_PACC_PPC)
 
 #------------------------------------------------------------------------------#
 # For Session 4
 
-session_bindEVACC_N <- rbind(session_bindEVACC_N, S4_EVACC_N)
-session_bindEVACC_PPC <- rbind(session_bindEVACC_PPC, S4_EVACC_PPC)
+session_bind_PACC_N <- rbind(session_bind_PACC_N, S4_PACC_N)
+session_bind_PACC_PPC <- rbind(session_bind_PACC_PPC, S4_PACC_PPC)
 
 #------------------------------------------------------------------------------#
 # For Session 5
 
-session_bindEVACC_N <- rbind(session_bindEVACC_N, S5_EVACC_N)
-session_bindEVACC_PPC <- rbind(session_bindEVACC_PPC, S5_EVACC_PPC)
+session_bind_PACC_N <- rbind(session_bind_PACC_N, S5_PACC_N)
+session_bind_PACC_PPC <- rbind(session_bind_PACC_PPC, S5_PACC_PPC)
 
 #------------------------------------------------------------------------------#
 # For Session 6
 
-
-session_bindEVACC_N <- rbind(session_bindEVACC_N, S6_EVACC_N)
-session_bindEVACC_PPC <- rbind(session_bindEVACC_PPC, S6_EVACC_PPC)
+session_bind_PACC_N <- rbind(session_bind_PACC_N, S6_PACC_N)
+session_bind_PACC_PPC <- rbind(session_bind_PACC_PPC, S6_PACC_PPC)
 
 
 # bind using cbind function
-session_bindEVACC_N <- cbind(block, session_bindEVACC_N, Part_N)
-session_bindEVACC_PPC <- cbind(block, session_bindEVACC_PPC, Part_PPC)
-
+session_bind_PACC_N <- cbind(block, session_bind_PACC_N, Part_N)
+session_bind_PACC_PPC <- cbind(block, session_bind_PACC_PPC, Part_PPC)
 #------------------------------------------------------------------------------#
 # ACC tendency between sessions (in graphs)
 
 # SHAM Stimulation
-g1 <- ggplot(data = session_bindEVACC_N,
+g1 <- ggplot(data = session_bind_PACC_N,
              aes(x = Blocks, y = accuracy, group = Participants_N, color = Participants_N)) + geom_line(size=1)
 g1 <- g1 + geom_point(size = 3)
-g1 <- g1 + ggtitle("SHAM stimulation accuracy in Executive vigilance task") + theme(plot.title = element_text(hjust=0.5))
+g1 <- g1 + ggtitle("SHAM stimulation accuracy in phasic alertness task") + theme(plot.title = element_text(hjust=0.5))
 plot(g1)
 
 # PPC Stimulation
-g2 <- ggplot(data = session_bindEVACC_PPC,
+g2 <- ggplot(data = session_bind_PACC_PPC,
              aes(x = Blocks, y = accuracy, group = Participants_PPC, color = Participants_PPC)) + geom_line(size=1)
 g2 <- g2 + geom_point(size = 3)
-g2 <- g2 + ggtitle("PPC stimulation accuracy in Executive vigilance task") + theme(plot.title = element_text(hjust=0.5))
+g2 <- g2 + ggtitle("PPC stimulation accuracy in phasic alertness task") + theme(plot.title = element_text(hjust=0.5))
 plot(g2)
